bfdcbdaf90a652397551f2529ef7e38d5a6a71bd
Yassine Bess <yazo-yazo@hotmail.com>
2022-07-20 17:31:12 +0100

    Updates

---
+++ precache-manifest.af9cc245888b027e45ee63f983eb7a28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b826938a8e78eec4cb48",
+    "url": "/Portfolio/static/css/main.f03c1d16.chunk.css"
+  },
+  {
+    "revision": "b826938a8e78eec4cb48",
+    "url": "/Portfolio/static/js/main.b826938a.chunk.js"
+  },
+  {
+    "revision": "c9e880f8674bdb92cbe8",
+    "url": "/Portfolio/static/css/1.fbf4288b.chunk.css"
+  },
+  {
+    "revision": "c9e880f8674bdb92cbe8",
+    "url": "/Portfolio/static/js/1.c9e880f8.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "39064856aac83d23ff555d03bb145a55",
+    "url": "/Portfolio/static/media/blog.39064856.png"
+  },
+  {
+    "revision": "508116fa1f206aa862dd64b0016a8ee7",
+    "url": "/Portfolio/index.html"
+  }
+];
